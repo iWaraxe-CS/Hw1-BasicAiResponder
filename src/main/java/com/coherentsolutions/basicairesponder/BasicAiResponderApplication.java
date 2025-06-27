a13a83f053c8be715aa6c1751f429aae757f56c0
iWaraxe <igor.waraxe@gmail.com>
2025-06-27 12:55:10 +0200

    Initial Commit

---
+++ src/main/java/com/coherentsolutions/basicairesponder/BasicAiResponderApplication.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.basicairesponder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAiResponderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAiResponderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coherentsolutions/basicairesponder/BasicAiResponderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.basicairesponder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAiResponderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
